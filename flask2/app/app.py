78f677355d1063fcfe97b516d2b7465ee1dca132
KaiFukushima0921 <kai2co2@gmail.com>
2023-11-23 11:44:22 +0900

    2周目 day2-2完了

--- flask2/app/app.py
+++ flask2/app/app.py
@@ -13,8 +13,5 @@ def hello():
 def index():
     name = request.args.get("keyword")
     okyo = ["色不異空","空不異色","色即是空","空即是色"]
-    return render_template("index.html",name=name,okyo=okyo)
+    return render_template("index.html",passed_keyword=name,okyo=okyo)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
