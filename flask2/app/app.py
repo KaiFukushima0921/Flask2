9ffa8a8bc234cbe68ff2c36516040f2bc3a9e54d
KaiFukushima0921 <kai2co2@gmail.com>
2023-11-23 11:22:02 +0900

    2周目 day2-1完了

--- flask2/app/app.py
+++ flask2/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -8,9 +8,12 @@ def hello():
     return "Hello world"
 
 
+@app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    name = request.args.get("keyword")
+    okyo = ["色不異空","空不異色","色即是空","空即是色"]
+    return render_template("index.html",name=name,okyo=okyo)
 
 
 if __name__ == "__main__":
