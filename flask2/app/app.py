66de0377a6762fed07e3f78bea7f94993d683643
KaiFukushima0921 <kai2co2@gmail.com>
2023-11-24 22:03:33 +0900

    2周目 day3完了

--- flask2/app/app.py
+++ flask2/app/app.py
@@ -4,14 +4,16 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello world"
-
-
-@app.route("/")
 @app.route("/index")
 def index():
     name = request.args.get("keyword")
     okyo = {'okyo1':'色不異空','okyo2':'空不異色','okyo3':'色即是空','okyo4':'空即是色'}
     return render_template("index.html",passed_keyword=name,okyo=okyo)
+
+
+@app.route("/hello",methods=["post"])
+def post():
+    name = request.form["name"]
+    okyo = {'okyo1':'色不異空','okyo2':'空不異色','okyo3':'色即是空','okyo4':'空即是色'}
+    return render_template("index.html", passed_keyword=name, okyo=okyo)
 
