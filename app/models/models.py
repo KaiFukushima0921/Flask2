504ba06a52cd62b480e4609d280c9627f7740211
KaiFukushima0921 <kai2co2@gmail.com>
2023-11-28 18:22:08 +0900

    test

---
+++ app/models/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
+from .database import Base
+from datetime import datetime
+
+
+class OnegaiContent(Base):
+    __tablename__ = 'task'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), unique=True)
+    body = Column(Text)
+    date = Column(DateTime, default=datetime.now())
+    done = Column(Boolean, default=False, nullable=False)
+
+
+    def __init__(self, name=None, body=None, date=None, done=None):
+        self.name = name
+        self.body = body
+        self.date = date
+        self.done = done
+
+
+    def __repr__(self):
+        return '<Title %r>' % (self.name)
