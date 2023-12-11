eb7a04900ab03285375e43cbf8a09ed62950a23e
KaiFukushima0921 <kai2co2@gmail.com>
2023-12-11 20:42:15 +0900

    day6完了

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template,request
-from .models.models import OnegaiContent
+from flask import Flask,render_template,request,session,redirect,url_for
+from app import key
+from hashlib import sha256
+from .models.models import OnegaiContent,User
 from .models.database import db_session
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = key.SECRET_KEY
 
 
 @app.route("/")
@@ -12,13 +15,16 @@ def index():
     name = request.args.get("keyword")
     after = request.args.get("after")
     done = request.args.get("done")
-    if after:
+    if "user_name" in session:
+        name = session["user_name"]
+        all_onegai = OnegaiContent.query.all()
+        return render_template("index.html",passed_keyword=name,all_onegai=all_onegai)
+    elif after:
         all_onegai = OnegaiContent.query.filter(OnegaiContent.date>=after).all()
     elif done:
         all_onegai = OnegaiContent.query.filter(OnegaiContent.done==1).all()
     else:
-        all_onegai = OnegaiContent.query.all()
-    return render_template("index.html",passed_keyword=name,all_onegai=all_onegai)
+        return redirect(url_for("top",status="logout"))
 
 """
 SQL文で書いた場合
@@ -42,13 +48,6 @@ select * from task where date >= after
 #     return render_template("index.html",passed_keyword=name,all_onegai=all_onegai)
 
 
-@app.route("/hello",methods=["POST"])
-def post():
-    name = request.form["name"]
-    all_onegai = OnegaiContent.query.all()
-    return render_template("index.html", passed_keyword=name, all_onegai=all_onegai)
-
-
 @app.route("/add",methods=["post"])
 def add():
     name = request.form["name"]
@@ -56,7 +55,7 @@ def add():
     content = OnegaiContent(name,body,datetime.now())
     db_session.add(content)
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
 
 
 @app.route("/update",methods=["post"])
@@ -65,7 +64,7 @@ def update():
     content.name = request.form["name"]
     content.body = request.form["body"]
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
 
 
 @app.route("/delete",methods=["post"])
@@ -75,7 +74,7 @@ def delete():
         content = OnegaiContent.query.filter_by(id=id).first()
         db_session.delete(content)
     db_session.commit()
-    return index()
+    return redirect(url_for("index"))
 
 
 @app.route("/done",methods=["post"])
@@ -86,17 +85,57 @@ def done():
         content.done = True
         # 67行目は「done = 1」でも実行できた
         db_session.commit()
-    return index()
+    return redirect(url_for("index"))
 
 
-# @app.route("/done",methods=["post"])
-# def done():
-#     name = request.form["name"]
-#     body = request.form["body"]
-#     content = OnegaiContent(name,body,datetime.now())
-#     db_session.add(content)
-#     db_session.commit()
-#     return index
+@app.route("/login", methods=["post"])
+def logion():
+    email = request.form["user_name"]
+    user = User.query.filter_by(user_name=email).first()
+    if user:
+        password = request.form["password"]
+        hashed_password = sha256((email + password + key.SALT).encode("utf-8")).hexdigest()
+        if user.hashed_password == hashed_password:
+            session["user_name"] = email
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("top",status="wrong_password"))
+    else:
+        return redirect(url_for("top",status="user_notfound"))
+    
+
+@app.route("/logout")
+def logout():
+    session.pop("user_name", None)
+    return redirect(url_for("top",status="logout"))
+    
+
+@app.route("/resister",methods=["post"])
+def resister():
+    email = request.form["user_name"]
+    user = User.query.filter_by(user_name=email).first()
+    if user:
+        return redirect(url_for("newcomer", status="exist_user"))
+    else:
+        password = request.form["password"]
+        hashed_password = sha256((email + password + key.SALT).encode("utf-8")).hexdigest()
+        user = User(email, hashed_password)
+        db_session.add(user)
+        db_session.commit()
+        session["user_name"] = email
+        return redirect(url_for("index"))
+    
+
+@app.route("/top")
+def top():
+    status = request.args.get("status")
+    return render_template("top.html",status=status)
+
+
+@app.route("/newcomer")
+def newcomer():
+    status = request.args.get("status")
+    return render_template("newcomer.html",status=status)
 
 
     
